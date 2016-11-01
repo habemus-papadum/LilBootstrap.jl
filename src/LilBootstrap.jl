225762b1b73ec8385ba62ae9e357d0e4f0b343e5
Nehal Patel <nehal@alum.mit.edu>
2016-11-01 13:57:54 -0400

    ++ attempt to reinstrument julia

--- src/LilBootstrap.jl
+++ src/LilBootstrap.jl
@@ -1,7 +1,14 @@
+__precompile__()
+
 module LilBootstrap
 
 const base_dir = normpath(joinpath(dirname(@__FILE__),".."))
 const ephemera = joinpath(base_dir,"deps/ephemera")
+const local_repos = joinpath(ephemera,"repos")
+const version_dir = "v$(Base.VERSION.major).$(Base.VERSION.minor)"
+const dot_julia = joinpath(ephemera,".julia")
+const compile_cache = joinpath(dot_julia, "lib",version_dir)
+
 
 """A json-like store of config
 
@@ -22,12 +29,12 @@ function clean()
   rm(ephemera;force=true, recursive=true)
 end
 
-"----------------------------------------------------"
+#----------------------------------------------------
 
 "Prelude"
 ⇶(xs,f) = [f(x) for x=xs]
 
-"----------------------------------------------------"
+#----------------------------------------------------
 
 
 """
@@ -35,23 +42,27 @@ bootboot: Inefficiently, redundantly, brute forces its ways to a working install
 <scaffolding: Make this modular, and steps idempotent, and redundancy checks quick>
 """
 function bootboot()
-  mkpath(ephemera)
+
+  #creates necessary directories
+  mkpath(joinpath(dot_julia,version_dir))
 
   repo_str(fork) = "$(fork[:repo]).jl"
   url(fork) = "https://github.com/lilinjn/$(repo_str(fork))"
-  local_path(fork) = joinpath(ephemera,"repos",repo_str(fork))
+  local_path(fork) = joinpath(local_repos,string(fork[:repo]))
+  pkg_path(fork) = joinpath(dot_julia, version_dir,string(fork[:repo]))
   runf(fork, cmd) = run(Cmd(cmd,dir=local_path(fork)))
-
   #shell out to git; Boycott LibGit2.jl for hardcoding Github specific behavior
   retrieve(fork) = begin
     lp = local_path(fork)
     print_with_color(:yellow, "$(fork[:repo])...\n")
     ispath(lp) || run(`git clone -b $(fork[:treeish]) $(url(fork)) $(lp)`)
+
     runf(fork, `git pull`)
+    symlink(lp, pkg_path(fork))
+
     fork
   end
 
-  build_config()
   print_with_color(:yellow, "Retrieving forks...\n")
   config[:forks]  ⇶ retrieve
 
@@ -63,18 +74,46 @@ function build_config()
   #technically, should be commit-ish, but that term is artless
   fork(repo, treeish="master") = Dict(:repo=>repo, :treeish=>treeish)
 
-  config[:forks] = [:Conda, :IJulia] ⇶ fork
+  config[:forks] = [:Conda, :IJulia, :JSON, :ZMQ, :Compat] ⇶ fork
   config
 end
 
+function __init__()
+  build_config()
+
+  # all hell breaks loose now
+  push!(LOAD_PATH, Pkg.dir()) #allow access to Pkgs from the host julia (I will regret this)
+  ENV["JULIA_PKGDIR"]=dot_julia
+  prepend!(Base.LOAD_CACHE_PATH,[compile_cache])
+  #=
+  Commentary on the current state of Julia:
+  I am fine with LOAD_PATH and LOAD_CACHE_PATH; I very much dislike
+  ENV["JULIA_PKGDIR"], and all things Pkg in general, to the point I even considered
+  `push!(LOAD_PATH, local_repos);ENV["JULIA_PKGDIR"]=tempname()`  However, I chose to be a little
+  less hardline, and the current version allows Pkg's from the host julia
+  config to (possibly) still work, albeit requiring a potential re-pre-compilation
+
+  After this module had loaded, Pkg.add, rm, etc could still work,
+  but inefficiently -- re-pulling in Pkgs already
+  available on the host julia.
+
+  But the whole point is that we are trying not use Pkg at all.
+
+  But there are times in which one finds themselves in a hacker fugue state, and just wants
+  to pull in code the quick and dirty way, rather than recommended "fork and hack" approach endorsed
+  by LilBootstrap.
+
+  As a practical sanity check/speed bump, any initial Pkg command will fail due to the lack of a METADATA dir,
+  asking the user to explicitly run Pkg.init() -- which is easy enough
+
+  And I very much like the idea of letting myself shoot my own foot off -- Now that is a good design!
+  =#
+
 end
 
+end
 
-
-
-
-
-"""
+#=
 Cleanup:
    * printing
-"""
+=#
