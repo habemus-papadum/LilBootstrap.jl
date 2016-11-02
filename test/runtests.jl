f96bdebf6721b128d91a52057260090512d831be
Nehal Patel <nehal@alum.mit.edu>
2016-11-02 19:18:51 -0400

    test zeromq

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,15 +7,16 @@ using Base.Test
 using LilBootstrap
 using PackagePrelude
 
-embedded = PackagePrelude.list_packages(forks=false)
+pkgs = PackagePrelude.list_packages(forks=false)
 print_with_color(:yellow, "Testing loading...\n")
-for e in embedded
+for e in pkgs
   print_with_color(:yellow, "$(e)...\n")
   eval(:(using $e))
 end
 
 print_with_color(:yellow, "Run tests...\n")
-for e in embedded
+push!(pkgs, :ZMQ)
+for e in pkgs
   print_with_color(:yellow, "$(e)...\n")
   test = joinpath(PackagePrelude.locate_package(e),"test", "runtests.jl")
   isfile(test) && include(test)
